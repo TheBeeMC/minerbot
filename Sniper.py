f31ef626fb1de0981a75c6e0853a675399009c89
TheBeeMC <thebeemcnetwork@gmail.com>
2018-10-12 04:40:30 +0700

    Update Sniper.py

--- Sniper.py
+++ Sniper.py
@@ -1,37 +1,57 @@
+# sniper.py
 import discord
+from discord.ext import commands
 import random
+import asyncio
 import os
+import subprocess
+import logging
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged in as')
-        print(self.user.name)
-        print(self.user.id)
-        print('------')
+command_prefix='+'
+bot = commands.Bot(command_prefix)
+description = 'sniper.py, coded by unpredictable'
+ 
+@bot.event
+async def on_message(message):
+    if message.content.startswith('+snipeadd'):
+            embed = discord.Embed(title="Let's walk you through the process of queueing a snipe.", description="DM the bot with 'beginqueue' to queue your snipe.", colour=0x1a94f0)
+            embed.set_author(name="Thanks for choosing sniper.py!", icon_url="")
+            embed.add_field(name="Enjoy your new name!", value="-trinity and iliyan")
+            embed.set_footer(text="sniper.py™ © coded by unpredictable")
+            await bot.send_message(message.channel, embed=embed)
 
-    async def on_message(self, message):
-        # we do not want the bot to reply to itself
-        if message.author.id == self.user.id:
-            return
+    if message.content.startswith('+help'):
+        embed=discord.Embed(title="+snipeadd: queues a snipe", description="+invite: Prints an invite link for the bot.", color=0x1a94f0)
+        embed.set_author(name='Commands', icon_url="")
+        embed.add_field(name="+help: Brings up list of commands", value="+info: Prints info about the bot.", inline=True)
+        embed.set_footer(text='coded by unpredictable')
+        await bot.send_message(message.channel, embed=embed)
 
-        if message.content.startswith('guess'):
-            await message.channel.send('Guess a number between 1 and 10000000.')
+    if message.content.startswith('+invite'):
+        await bot.send_message(message.channel, "**Here's the invite for the bot:** INVITE")
+        print('Bot has been added to new server')
 
-            def is_correct(m):
-                return m.author == message.author and m.content.isdigit()
+    if message.content.startswith('?tag twitter'):
+        await bot.send_message(message.channel, "https://twitter.com/PaintToolApp")
 
-            answer = random.randint(1, 10000000)
+async def status_task():
+    while True:
+        await bot.change_presence(game=discord.Game(name="with og names"))
+        await asyncio.sleep(10)
+        await bot.change_presence(game=discord.Game(name="+help | coded by ili"))
+        await asyncio.sleep(10)
+        await bot.change_presence(game=discord.Game(name="with my sniper"))
+        await asyncio.sleep(10)
+        await bot.change_presence(game=discord.Game(name="with a fidget spinner"))
+        await asyncio.sleep(10)
 
-            try:
-                guess = await self.wait_for('message', check=is_correct, timeout=5.0)
-            except asyncio.TimeoutError:
-                return await message.channel.send('Sorry, you suck Alexa {}.'.format(answer))
-
-            if int(guess.content) == answer:
-                await message.channel.send('You are right!')
-            else:
-                await message.channel.send('Oops. It is actually {}.'.format(answer))
-
-client = MyClient()
+@bot.event
+async def on_ready():
+    print('sniper.py coded by unpredictable')
+    print('------')
+    print('INFO')
+    print('------')
+    print('Logged in as: ' + bot.user.name + ', ' + bot.user.id)
+    print('------')
 
 client.run(os.getenv('TOKEN'))
