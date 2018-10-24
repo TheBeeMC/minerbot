68726be8645d3d0b4a904ba4b2dbcc4a89d9267f
TheBeeMC <thebeemcnetwork@gmail.com>
2018-10-25 05:49:05 +0700

    Update sniper.py

--- sniper.py
+++ sniper.py
@@ -41,7 +41,7 @@ async def on_message(message):
 
 @bot.event
 async def on_ready():
-    await bot.change_status(game=discord.Game(name='What is your highest score?'))
+    await bot.change_status(game=discord.Game(name='Minecraft'))
     print('sniper.py coded by unpredictable')
     print('------')
     print('INFO')
