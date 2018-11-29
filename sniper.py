c54d556798f4dd4a8f01646111b35b472b572f99
TheBeeMC <thebeemcnetwork@gmail.com>
2018-11-29 06:38:05 -0500

    Update sniper.py

--- sniper.py
+++ sniper.py
@@ -1,28 +1,20 @@
-# sniper.py
 import discord
-from discord.ext import commands
-import random
-import asyncio
 import os
-import subprocess
-import logging
 
-bot = commands.Bot(command_prefix='/')
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
 
- 
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
         if message.content.startswith('!hello'):
-            await message.channel.send('Hello {0.author.mention}'.format(message
-        
+            await message.channel.send('Hello {0.author.mention}'.format(message))
 
-
-
-@bot.event
-async def on_ready():
-    await bot.change_presence(game=discord.Game(name="C:\Users\Discord"))
-    print('Miner Bot™ @ coded by Captain#2713')
-    print('------')
-    print('INFO')
-    print('------')
-    print('Logged in as: ' + bot.user.name + ', ' + bot.user.id)
-        
-bot.run(os.getenv('TOKEN'))
+client = MyClient()
+client.run(os.getenv('TOKEN'))
