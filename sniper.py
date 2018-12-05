28a86c5f86bb936a37b762e490bc7a6adc52fb1b
TheBeeMC <thebeemcnetwork@gmail.com>
2018-12-05 18:59:49 -0500

    Update sniper.py

--- sniper.py
+++ sniper.py
@@ -55,8 +55,8 @@ async def on_message(message):
         embed.set_footer(text='Thread posted by Captain#2713')
         await bot.send_message(message.channel, embed=embed)         
 
-    if message.content.startswith('/w3isnip13einfos'):
-        await bot.send_message(message.channel, "The name `Brad` is being dropped on 21/12/2018 @ 09:22:56 and has been queued.")
+    if message.content.startswith('/SNIPEw3isnip13einfos'):
+        await bot.send_message(message.channel, "The name `Catacombs` has been sniped on a unmigrated account.")
 
 async def status_task():
     while True:
