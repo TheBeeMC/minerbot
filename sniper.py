cb9954d6cb0e915266f837147c0b1d2fd80948c0
TheBeeMC <thebeemcnetwork@gmail.com>
2018-12-13 17:42:12 -0500

    Update sniper.py

--- sniper.py
+++ sniper.py
@@ -49,7 +49,7 @@ async def on_message(message):
 
 @bot.event
 async def on_ready():
-    await bot.change_status(game=discord.Game(name='Testing'))
+    await bot.change_status(game=discord.Game(name='Sniping name...'))
     print('sniper.py coded by unpredictable')
     print('------')
     print('INFO')
