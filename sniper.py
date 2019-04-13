889c832394548f931e6ee75435584b56d06b95d3
TheBeeMC <thebeemcnetwork@gmail.com>
2019-04-13 08:16:05 -0400

    Update sniper.py

--- sniper.py
+++ sniper.py
@@ -31,7 +31,9 @@ async def on_message(message):
              
     if message.content.startswith('.setsnipeusername: '):
         await bot.send_message(message.channel, "The name has been queued using the payment: Force command. Queued since the last 24 hours. ")        
-      
+ 
+     if message.content.startswith('.failedsnipe'):
+        await bot.send_message(message.channel, "@everyone USERNAME SET AS: Upstate has not been succesfuly sniped due to reason: Error while executing the snipe")                
       
     if message.content.startswith('.successsnipe'):
         await bot.send_message(message.channel, "@everyone USERNAME SET AS: Upstate has been succesfuly sniped using a unmigrated account.")                
