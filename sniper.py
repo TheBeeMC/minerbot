ab75f1d1671be91ec15c539d8438c62f8dee95fe
TheBeeMC <thebeemcnetwork@gmail.com>
2019-04-13 03:41:54 -0400

    Update sniper.py

--- sniper.py
+++ sniper.py
@@ -14,9 +14,9 @@ description = 'sniper.py, coded by unpredictable'
 @bot.event
 async def on_message(message):
     if message.content.startswith('.ddss confirm'):
-            embed = discord.Embed(title="public class Discord Confirmation", description="captchaDetails.put(Human Verification)", colour=0x1a94f0)
-            embed.add_field(name="System.out.println(==> Please wait until a staff moves you in)", value="Posted on: 12/17/2018", inline=True)
-            embed.set_author(name="import java.mcducksniper;", icon_url="")
+            embed = discord.Embed(title="public class name.drop", description="Details.put(Username)", colour=0x1a94f0)
+            embed.add_field(name="System.out.println(==> Upstate is coming up on namemc and is not queued on chearful)", value="Posted on: 4/13/2019", inline=True)
+            embed.set_author(name="Name Drop Alert @here", icon_url="")
             embed.set_footer(text="")
             await bot.send_message(message.channel, embed=embed)
 
